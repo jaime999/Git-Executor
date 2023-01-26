68858b938b916b73967b5c4022bff19d78777f17
jaime999 <martinezsanchezjaime@gmail.com>
2023-01-27 00:13:31 +0100

    Añadido consumer en el server

--- server/server.js
+++ server/server.js
@@ -8,16 +8,18 @@ const port = 3000
 const jsonParser = bodyParser.json()
 
 app.post('/', jsonParser, async (req, res) => {
-  await producer.connect()             // ++
+  await producer.connect()
+  console.log('Esto es el body', req.body)             // ++
   try {
-   const responses = await producer.send({
-      topic: process.env.TOPIC,
+   await producer.send({
+      topic: process.env.TOPIC_PRODUCER,
       messages: [{
          // Name of the published package as key, to make sure that we process events in order
          // The message value is just bytes to Kafka, so we need to serialize our JavaScript
          // object to a JSON string. Other serialization methods like Avro are available.
          value: JSON.stringify({
-            repository: req.body.repository
+            repository: req.body.repository,
+            command: req.body.command
          })
       }]
    })
@@ -31,36 +33,36 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-// const consumer = kafka.consumer({
-//    groupId: process.env.GROUP_ID
-// })
+const consumer = kafka.consumer({
+   groupId: process.env.GROUP_ID
+})
 
-// const main = async () => {
-//    await consumer.connect()
-//    await consumer.subscribe({
-//       topic: process.env.TOPIC,
-//       fromBeginning: true
-//    })
-//    await consumer.run({
-//       eachMessage: async ({ topic, partition, message }) => {
-//          console.log('Received message', {
-//             topic,
-//             partition,
-//             value: message.value.toString()
-//          })
+const main = async () => {
+   await consumer.connect()
+   await consumer.subscribe({
+      topic: process.env.TOPIC_CONSUMER,
+      fromBeginning: true
+   })
+   await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+         console.log('Received message', {
+            topic,
+            partition,
+            value: message.value.toString()
+         })
 
-//          // console.log(message)
+         // console.log(message)
          
-//       }
-//    })
-// }
+      }
+   })
+}
 
-// main().catch(async error => {
-//    console.error(error)
-//    try {
-//       await consumer.disconnect()
-//    } catch (e) {
-//       console.error('Failed to gracefully disconnect consumer', e)
-//    }
-//    process.exit(1)
-// })
+main().catch(async error => {
+   console.error(error)
+   try {
+      await consumer.disconnect()
+   } catch (e) {
+      console.error('Failed to gracefully disconnect consumer', e)
+   }
+   process.exit(1)
+})
